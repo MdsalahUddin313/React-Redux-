bec4b369c56f199cacd604986ed73ad91ec08dad
MdsalahUddin313 <nabidbhuia314@gmail.com>
2022-06-17 13:38:22 +0600

    --- Auto Git Commit ---

--- src/components/Header_comp/Navbar.jsx
+++ src/components/Header_comp/Navbar.jsx
@@ -1,13 +1,39 @@
 import React from "react";
+import { Nav, Container } from "react-bootstrap";
 
 const Navbar = () => {
   return (
-    <div>
-      <a href="/">Home</a>
-      <a href="#">About</a>
-      <a href="#">Cart</a>
-    </div>
+    <>
+      <Container>
+        <Nav className="justify-content-center bg-secondary">
+          <Nav.Item as="li">
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link href="/counter">Counter</Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item as="li">
+            <Nav.Link href="/">Todo List</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link href="/">Calculator</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link href="/">Ecommerce</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
+    </>
   );
 };
 
 export default Navbar;
+/*
+<a >Home</a>
+      <a href="/counter">Counter App</a>
+      <a href="#">Todo List</a>
+      <a href="#">Calculator</a>
+      <a href="#">Ecommerce website</a>
+      
+*/
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,20 +4,6 @@ const Home = () => {
   return (
     <div>
       <p>home page</p>
-      <ul>
-        <li>
-          <a href="/counter">Increment Decrement</a>
-        </li>
-        <li>
-          <a href="#">Todo List app</a>
-        </li>
-        <li>
-          <a href="#">Calculator</a>
-        </li>
-        <li>
-          <a href="#">Ecommerce website</a>
-        </li>
-      </ul>
     </div>
   );
 };
