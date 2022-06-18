9dc0f399714244656af114e22897f5be6e1befe3
MdsalahUddin313 <nabidbhuia314@gmail.com>
2022-06-18 09:49:19 +0600

    --- Auto Git Commit ---

--- src/components/ToDos/ADD_Contacts.js
+++ src/components/ToDos/ADD_Contacts.js
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const ADD_Contacts = () => {
+  const [contactNames, setContactNames] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const numberOfContacts = useSelector(
+    (state) => state.contactsList.contacts.length
+  );
+  const dispatch = useDispatch();
+  //console.log(numberOfContacts);
+  //console.log({ contactNames, phoneNumber });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const timestamp = new Date().getTime();
+    const timestamp_string = timestamp.toString();
+    const contactList = {
+      id: numberOfContacts + 1,
+      contactNames,
+      phoneNumber,
+      timestamp_string,
+    };
+    console.log(contactList);
+  };
   return (
     <div>
       <h3>Add Contacts</h3>
-      <form>
-        <p>Enter Name</p>
-        <p>Enter Contacts</p>
+      <form onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Enter Name"
+            required
+            value={contactNames}
+            onChange={(e) => setContactNames(e.target.value)}
+          ></input>
+          <br />
+          <label htmlFor="phone-number">Mobile Number</label>
+          <input
+            type="text"
+            id="phone-number"
+            placeholder="Enter Mobile Number"
+            required
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          ></input>
+        </div>
       </form>
-      <button>Submit</button>
+      <button type="submit">(+) Add Contact Number </button>
     </div>
   );
 };
